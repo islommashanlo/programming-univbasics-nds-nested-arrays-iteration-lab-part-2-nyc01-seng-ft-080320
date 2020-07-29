60f4f1dc221cda48da18930e55e5f1d234c1b6a9
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-29 20:12:24 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ array_index = 0
 minimums = []
   while array_index < src.length do
     element_index = 0
-    while element_index < src[array_index]
+    while element_index < src[array_index].length do
       minimum_value = src[array_index].min
       element_index += 1
     end
