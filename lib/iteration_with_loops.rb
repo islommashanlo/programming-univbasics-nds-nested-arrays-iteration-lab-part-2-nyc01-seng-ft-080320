8bfd02eb3da9b34fdcb5fcfd82e2e60eb5c44701
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-29 19:43:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,4 +4,7 @@ def find_min_in_nested_arrays(src)
 array_index = 0
   while array_index < src.length do
     element_index = 0
+    while element_index < src[array_index].length do
+      
+    end
 end
